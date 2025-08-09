19a13766caaaad812131232eba2c36e021642f9a
tlhololaiza <tshwanetlholo7@gmail.com>
2025-08-09 23:24:06 +0200

    Fixed Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Text from '../Text/Text';
 import Button from '../Button/Button';
+import './Header.css'; 
 
 interface HeaderProps {
   isAuthenticated: boolean;
